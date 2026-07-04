e112ac33054e59cc50a492b1b11414e38dd1d1b7
Pedro Barros <pedrobarros0101@outlook.com>
2026-07-04 18:36:30 -0300

    feat: add more tests to repository library

--- tests/FCG.Catalog.Worker.Tests/Repositories/LibraryRepositoryTest.cs
+++ tests/FCG.Catalog.Worker.Tests/Repositories/LibraryRepositoryTest.cs
@@ -1,5 +1,6 @@
 ﻿using CommonTestUtilities.Database;
 using CommonTestUtilities.Entities;
+using FCG.Catalog.Worker.Infrastructure.Repositories;
 using FCG.Catalog.Worker.Tests.Fixture;
 using System;
 using System.Collections.Generic;
@@ -21,7 +22,7 @@ public class LibraryRepositoryTest
     }
 
     [Fact]
-    public void LibraryRepository_Shrould_GetLibraryByUserId()
+    public async Task LibraryRepository_Shrould_GetLibraryByUserId()
     {
         Guid userId = Guid.NewGuid();
         var game = _gameBuilder.GenerateGame();
@@ -29,12 +30,27 @@ public class LibraryRepositoryTest
         category.Games.Add(game);
         var library = _libraryBuilder.GenerateLibrary();
         library.UserId = userId;
-        var context = ContextBuilder.GenerateContext(categories: [category], games: [game], libraries: [library]);
+        var context = await ContextBuilder.GenerateContext(categories: [category], libraries: [library]);
 
         var repository = new LibraryRepository(context);
 
-        var foundLibrary = repository.GetLibraryByUserId(library.UserId);
-        Assert.Single(library.Games);
-        Assert.Equal(game.Name, library.Games.ToList()[0].Name);
+        var foundLibrary = await repository.GetLibraryByUserId(userId);
+        Assert.NotNull(foundLibrary);
+    }
+    [Fact]
+    public async Task LibraryRepository_Shrould_GetLibraryNull()
+    {
+        Guid userId = Guid.NewGuid();
+        var game = _gameBuilder.GenerateGame();
+        var category = _categoryBuilder.GenerateCategory();
+        category.Games.Add(game);
+        var library = _libraryBuilder.GenerateLibrary();
+        library.UserId = Guid.NewGuid();
+        var context = await ContextBuilder.GenerateContext(categories: [category], libraries: [library]);
+
+        var repository = new LibraryRepository(context);
+
+        var foundLibrary = await repository.GetLibraryByUserId(userId);
+        Assert.Null(foundLibrary);
     }
 }

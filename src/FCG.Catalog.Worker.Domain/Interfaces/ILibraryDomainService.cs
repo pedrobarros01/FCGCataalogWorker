1a6a1d9c9fc338badedc3d13f57b26355e1d2c01
Pedro Barros <pedrobarros0101@outlook.com>
2026-07-04 20:07:30 -0300

    feat: create domain service of library

---
+++ src/FCG.Catalog.Worker.Domain/Interfaces/ILibraryDomainService.cs
@@ -0,0 +1,12 @@
+﻿using FCG.Catalog.Worker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Catalog.Worker.Domain.Interfaces;
+
+public interface ILibraryDomainService
+{
+    Task<Library> AddGame(Library library, Guid gameId);
+    Task<Library?> GetLibraryByUserId(Guid userId);
+}
---
+++ src/FCG.Catalog.Worker.Domain/Services/LibraryDomainService.cs
@@ -0,0 +1,30 @@
+﻿using FCG.Catalog.Worker.Domain.Entities;
+using FCG.Catalog.Worker.Domain.Exceptions;
+using FCG.Catalog.Worker.Domain.Interfaces;
+using FCG.Catalog.Worker.Domain.Interfaces.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FCG.Catalog.Worker.Domain.Services;
+
+public class LibraryDomainService : ILibraryDomainService
+{
+    private readonly ILibraryRepository _libraryRepository;
+    private readonly IGameRepository _gameRepository;
+
+    public LibraryDomainService(ILibraryRepository libraryRepository, IGameRepository gameRepository)
+    {
+        _libraryRepository = libraryRepository;
+        _gameRepository = gameRepository;
+    }
+
+    public async Task<Library> AddGame(Library library, Guid gameId)
+    {
+        Game game = await _gameRepository.GetById(gameId) ?? throw new BusinessException("Jogo não encontrado");
+        library.AddGame(game);
+        return library;
+    }
+
+    public async Task<Library?> GetLibraryByUserId(Guid userId) => await _libraryRepository.GetLibraryByUserId(userId);
+}
